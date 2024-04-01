c4569c8afcb4641595efe4266e730ac648bee2aa
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-04-01 10:03:38 -0700

    added PATCH endpoint for questions

--- app/api/quizzes/questions/[id]/route.tsx
+++ app/api/quizzes/questions/[id]/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/prisma/client';
+import { partialQuestionSchema } from '../schema';
 
 export async function GET(
   request: NextRequest,
@@ -12,3 +13,20 @@ export async function GET(
   if (!questions) return NextResponse.json({ error: 'No questions found' }, { status: 404 });
   return NextResponse.json(questions, { status: 200 });
 }
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: ReqParams) {
+    const body = await request.json();
+    const validation = partialQuestionSchema.safeParse(body);
+    if (!validation.success) return NextResponse.json(validation.error.errors, { status: 404 });
+    const updatedQuestion = await prisma.question.update({
+      where: {
+        id: parseInt(params.id)
+      },
+      data: {
+        ...body
+      }
+    });
+    return NextResponse.json(updatedQuestion, { status: 200 });
+  }
