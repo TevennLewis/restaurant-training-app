b713f13e721390d9e2a207d616bbe9ada1fd4b3b
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-04-06 12:49:13 -0700

    added partialQuizSchema and /quiz/:id PATCH route

--- app/api/quizzes/[id]/route.tsx
+++ app/api/quizzes/[id]/route.tsx
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/prisma/client';
-import { quizSchema } from '../schema';
+import { partialQuizSchema } from '../schema';
 
 export async function GET(
   request: NextRequest,
@@ -14,4 +14,19 @@ export async function GET(
     return NextResponse.json(quiz, { status: 200 });
   }
 
-  
+  export async function PATCH(
+    request: NextRequest,
+    { params }: ReqParams) {
+      const body = await request.json();
+      const validation = partialQuizSchema.safeParse(body);
+      if (!validation.success) return NextResponse.json(validation.error.errors, { status: 404 });
+      const updatedQuiz = await prisma.quiz.update({
+        where: {
+          id: parseInt(params.id)
+        },
+        data: {
+          ...body
+        }
+      });
+      return NextResponse.json(updatedQuiz, { status: 200 });
+    }
