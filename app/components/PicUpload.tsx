1f3d1e2dd223db13d290e516860ba0d22f817c31
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-04-08 17:05:07 -0700

    got img to correctly update member profilePic on upload

--- app/components/PicUpload.tsx
+++ app/components/PicUpload.tsx
@@ -9,7 +9,17 @@ interface CloudinaryResult {
 
 const PicUpload = () => {
   const [publicId, setPublicId] = useState('');
-
+  const memberId = '1';
+  useEffect(() => {
+    if (!publicId) return;
+    fetch(`/api/members/${memberId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ profilePic: publicId })
+    });
+  }, [publicId]);
   return (
     <CldUploadWidget 
       uploadPreset='r8oketi1'
